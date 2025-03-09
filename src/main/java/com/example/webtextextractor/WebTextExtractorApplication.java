0c376b4c4968bb6e50612f0c087fe8287c981e5d
y.farber <yulia@mabaya.com>
2025-03-09 12:27:13 +0200

    Initial commit

---
+++ src/main/java/com/example/webtextextractor/WebTextExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.webtextextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTextExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTextExtractorApplication.class, args);
+	}
+
+}
